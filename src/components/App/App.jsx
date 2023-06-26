60d422f0dca980bcdeb3f6c29ea91a83b52ce849
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-27 00:15:55 +0300

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import Expresso from "../Expresso";
+import { Container } from "./App.styled";
+
+export const App = () => {
+  return (
+    <Container>
+      <Expresso />
+    </Container>
+  );
+};
