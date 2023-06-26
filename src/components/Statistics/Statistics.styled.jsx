4c7040f21bbfd711d88819d4f6f9ae307a5fed69
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-27 00:44:11 +0300

    Create Statistics.styled.jsx

---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const Text = styled.span`
+    font-size: 20px;
+    display: block;
+    margin-bottom: 10px;
+    &:hover, &:focus {
+        color: #4CAF50;
+    }
+`;
