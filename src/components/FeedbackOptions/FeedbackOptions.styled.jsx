401b6dc05cd71f791da4268ef2c228f2d4ed5157
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-27 00:43:32 +0300

    Create FeedbackOptions.styled.jsx

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+
+export const Buttons = styled.button`
+    display: inline-block;
+    padding: 10px 30px;
+    font-size: 16px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    margin: 24px 20px;
+    cursor: pointer;
+    border-radius: 30px;
+    box-shadow: 0px 1px 6px rgba(46, 47, 66, 0.08), 0px 1px 1px rgba(46, 47, 66, 0.16), 0px 2px 1px rgba(46, 47, 66, 0.08);
+    &:hover, &:focus {
+        color: rgb(105, 103, 103);
+        background-color: #77c17a;
+    }
+`;
