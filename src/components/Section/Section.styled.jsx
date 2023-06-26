89edffc71f79893bd7e00967e60d1107ab6bdd15
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-27 00:44:00 +0300

    Create Section.styled.jsx

---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const Title = styled.h2`
+    font-weight: 900;
+    font-size: 36px;
+    text-align: center;
+    margin-bottom: 25px;
+`;
+
+export const Box = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    column-gap: 70px;
+    max-width: 1100px;
+    margin: 0 auto;
+    border: 2px solid green;
+`;
