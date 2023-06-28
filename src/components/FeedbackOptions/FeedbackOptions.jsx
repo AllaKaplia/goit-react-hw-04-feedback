90acdde97164933c1821223996e77dd181d37c7a
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-29 00:41:25 +0300

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import { Buttons } from './FeedbackOptions.styled';
 
 const FeedbackOptions = ({ options, addFeedback }) => {
-    return (
-      <div>
-        {options.map(option => (
-          <Buttons
-            key={option}
-            onClick={() => addFeedback(option)}
-          >
-            {option}
-          </Buttons>
-        ))}
-      </div>
-    );
-  };
+  return (
+    <div>
+      {options.map((option, index) => (
+        <Buttons key={index} onClick={() => addFeedback(option)}>
+          {option}
+        </Buttons>
+      ))}
+    </div>
+  );
+};
+
 
 export default FeedbackOptions;
